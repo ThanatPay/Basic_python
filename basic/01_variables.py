3d4fdb25ae8aa9f46ca80bc2293c881f7e315fc6
ThanatPay <t.payatsuporn@gmail.com>
2023-08-01 00:41:16 +0700

    Update basic

---
+++ basic/01_variables.py
@@ -0,0 +1,20 @@
+#integer variable
+i1=1
+i2=2
+print('type :', type(i1))
+
+#float variable
+f1=1.0
+f2=2.0
+print('type :', type(f1))
+
+#string variable
+s1='Hello'
+s2='World'
+print('type :', type(s1))
+
+#boolean variable
+b1=True
+b2=False
+print('type :', type(b1))
+
