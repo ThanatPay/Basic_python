e74e75458cb484708c163db7dc6da91cdda5912b
ThanatPay <t.payatsuporn@gmail.com>
2023-08-05 18:00:46 +0700

    Update basic

---
+++ basic/04_input.py
@@ -0,0 +1,19 @@
+# input from user
+a=input('Enter input:')
+print(a)
+# when you run this code and enter 18
+# Enter input: 18
+# 18
+print(type(a)) # str
+
+# if you want to input type int 
+b=int(input('Enter input:'))
+print(b)
+print(type(b)) # int
+
+# program that can find area of a square
+# an input side from user
+side=int(input('Enter side:'))
+print('Square area:',side*side)
+# Enter side: 3
+# Square area: 9
