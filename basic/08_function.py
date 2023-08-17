3008318c88f2c8b0a35c93d3cd0e68da617ef150
ThanatPay <t.payatsuporn@gmail.com>
2023-08-18 00:55:51 +0700

    Update basic

--- basic/08_function.py
+++ basic/08_function.py
@@ -43,14 +43,35 @@ print(cylinder_volume(2,2))
 # named arguments
 print(cylinder_volume(radius=3,height=3))
 
-# global and local variables
+# global vs local variables
+
+# global and local variables have the same name. but not the same variable
+# global variable is definded outside function 
+value=2
+# local variable is definded in function
 def two_times(value):
     print("local value:",value)
     value=2*value
     print("local value:",value)
-    return value
 
-value=2
 print("global value:",value)
 two_times(value=value)
 print("global value:",value)
+
+# can defind global variable in function use global
+def show_value(): 
+    global value1
+    global value2
+    value2=2
+    print("Print value1 in function:",value1)
+    print("Print value1 in function:",value2)
+
+
+# can't print value1 because the function defind value1 but can't set value
+value1=1 # You must set value to value1
+print("Print value1 outside function:",value1)
+# can't print value2 outside function becasue value2 is local variable
+# print("Print value1 outside function:",value2)
+show_value()
+
+
