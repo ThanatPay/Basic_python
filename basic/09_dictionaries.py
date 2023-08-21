e76aa520b64bff620fb2588214eebdddb6101529
ThanatPay <t.payatsuporn@gmail.com>
2023-08-22 00:11:20 +0700

    Update basic

---
+++ basic/09_dictionaries.py
@@ -0,0 +1,48 @@
+# dictionaries
+# store data on key:value format
+Dict = {"Alison": "Becker",
+        "Ibrahima": "Konate",
+        "Jordan": "Henderson",
+        "Mohamed": "Salah"}
+print(Dict)
+print(type(Dict))
+# you can get value from dict[key]
+print(Dict['Alison'])
+
+# update dict
+# The key may or may not exist in the dict
+Dict['Roberto']='Firmino'
+print(Dict)
+# using update function
+Dict.update({"Roberto": "carlos",
+             "Sadio": "Mane"})
+print(Dict)
+# using del
+del Dict['Roberto']
+print(Dict)
+
+# not everything can be used as a key
+# Lists cannot be used as keys, but tuples, numbers, and strings can
+# Dict[['Steven']]='Gerrard'
+
+# if you use get value from key method and return error
+# if key doesn't exist in dict
+# print(Dict['alison'])
+# using get function
+print(Dict.get('Alison'))
+print(Dict.get('alison'))
+print(Dict.get('alison',1))
+
+# remove key but return function
+# using pop function
+print(Dict.pop('Sadio'))
+print(Dict)
+
+# get key/value/item in dict
+print(Dict.keys())
+print(Dict.values())
+print(Dict.items())
+
+# Iteraring through Dicrionary
+for key in Dict:
+    print(key)
